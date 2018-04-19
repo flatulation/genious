8099760e836e1d2a1006468090c3a395b22c8ede
flatulation <36717206+flatulation@users.noreply.github.com>
2018-04-20 00:24:24 +0100

    change the create file * don't ignore things if they already exist * the combination of columns is unique and not null, but they are not part of the primary key * truncate the temporary table, don't drop it

--- src/db/pairs/sql/create.sql
+++ src/db/pairs/sql/create.sql
@@ -1,22 +1,22 @@
-CREATE TABLE IF NOT EXISTS pairs (
+CREATE TABLE pairs (
     user_id bigint,
-    first text,
-    second text,
-    third text,
-    PRIMARY KEY (user_id, first, second, third)
+    first text NOT NULL,
+    second text NOT NULL,
+    third text NOT NULL,
+    UNIQUE (user_id, first, second, third)
 );
 
-CREATE INDEX IF NOT EXISTS pairs_first_second_third 
+CREATE INDEX pairs_first_second_third 
 ON pairs (first, second, third);
 
-CREATE INDEX IF NOT EXISTS pairs_third
+CREATE INDEX pairs_third
 ON pairs (third);
 
 CREATE FUNCTION get_third_word(text, text) RETURNS text AS $$
     DECLARE
         third text;
     BEGIN
-        CREATE TEMP TABLE filtered AS
+        CREATE TEMP TABLE IF NOT EXISTS filtered AS
         SELECT p.third 
         FROM pairs p 
         WHERE first = $1 AND second = $2;
@@ -25,7 +25,7 @@ CREATE FUNCTION get_third_word(text, text) RETURNS text AS $$
         INTO third
         FROM filtered f TABLESAMPLE SYSTEM_ROWS(1);
 
-        DROP TABLE filtered;
+        TRUNCATE filtered;
 
         RETURN third;
     END;
