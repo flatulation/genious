5aaad69908295a623a4bcdc6b0edda7ba400060f
flatulation <36717206+flatulation@users.noreply.github.com>
2018-04-20 00:41:17 +0100

    declare in an object literal

--- src/db/pairs/index.js
+++ src/db/pairs/index.js
@@ -1,8 +1,8 @@
 const db = require('..');
 const sql = require('../sql');
 
-module.exports = exports = {};
-
-module.exports.create = async () => {
-  return db.none(sql.create);
+module.exports = exports = {
+  create: async () => {
+    return db.none(sql.create);
+  }
 };
