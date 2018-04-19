e39d8c96c87606d29106093a30ce8dac7ca1f4b9
flatulation <36717206+flatulation@users.noreply.github.com>
2018-04-20 00:20:47 +0100

    fix style issues

--- src/logger.js
+++ src/logger.js
@@ -1,17 +1,15 @@
 const winston = require('winston');
 
-const logger = winston.createLogger({
-    transports: [
-        new winston.transports.Console,
-    ],
-    exceptionHandlers: [
-        new winston.transports.Console,
-    ],
-    format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp(),
-        winston.format.printf(info => `${info.timestamp} - ${info.level}: ${info.message}`),
-    ),
-})
-
-module.exports = exports = logger;
+module.exports = exports = winston.createLogger({
+  transports: [
+    new winston.transports.Console,
+  ],
+  exceptionHandlers: [
+    new winston.transports.Console,
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.printf(info => `${info.timestamp} - ${info.level}: ${info.message}`),
+  ),
+});
